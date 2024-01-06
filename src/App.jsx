52669493619634dfee2997b9dfefd766d1b667fc
“GeorgiDS9git config --global user.name GeorgiDS9git config --global user.email georgianachiriacweb@gmail.comgit config --global user.email georgianachiriacweb@github.com <georgianachiriacweb@gmail.com>
2024-01-06 18:28:23 -0500

    display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 import data from "./data";
+import React from "react";
+import {
+  FaGolfBall,
+  FaMountain,
+  FaBookOpen,
+  FaBinoculars,
+  FaChevronLeft,
+  FaChevronRight,
+  FaQuoteRight,
+} from "react-icons/fa";
 
 const App = () => {
-  return <h2>Reviews App</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, description } = data[index];
+  return (
+    <main>
+      <article className="review"></article>
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4>{name}</h4>
+      <p>{job}</p>
+      <p>{description}</p>
+    </main>
+  );
 };
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,31 +1,35 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "lakshmi patel",
     job: "web developer",
     image: "https://course-api.com/images/people/person-1.jpeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     id: 2,
-    name: "anna johnson",
+    name: "latoya collins",
     job: "web designer",
     image: "https://course-api.com/images/people/person-2.jpeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     id: 3,
-    name: "peter jones",
+    name: "tim hopkins",
     job: "intern",
     image: "https://course-api.com/images/people/person-4.jpeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     id: 4,
-    name: "bill anderson",
+    name: "brandon andrews",
     job: "the boss",
     image: "https://course-api.com/images/people/person-3.jpeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
 ];
 
