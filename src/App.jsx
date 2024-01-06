618068bcebcd94107a24f0e64df250b2c24f8eb0
“GeorgiDS9git config --global user.name GeorgiDS9git config --global user.email georgianachiriacweb@gmail.comgit config --global user.email georgianachiriacweb@github.com <georgianachiriacweb@gmail.com>
2024-01-06 17:43:03 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import data from "./data";
+
+const App = () => {
+  return <h2>Reviews App</h2>;
+};
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image: "https://course-api.com/images/people/person-1.jpeg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image: "https://course-api.com/images/people/person-2.jpeg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image: "https://course-api.com/images/people/person-4.jpeg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image: "https://course-api.com/images/people/person-3.jpeg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
+
+export default reviews;
