3a2ffb4303c21be6386a4a0563afd5c40b9a980b
“GeorgiDS9git config --global user.name GeorgiDS9git config --global user.email georgianachiriacweb@gmail.comgit config --global user.email georgianachiriacweb@github.com <georgianachiriacweb@gmail.com>
2024-01-08 20:37:25 -0500

    add index logic

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,63 @@ import {
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, description } = data[index];
+
+  const checkIndex = (index) => {
+    if (index > data.length - 1) {
+      return 0;
+    }
+    if (index < 0) {
+      return data.length - 1;
+    }
+    return index;
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * data.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkIndex(randomNumber));
+  };
+
   return (
     <main>
-      <article className="review"></article>
-      <div className="img-container">
-        <img src={image} alt={name} className="person-img" />
-        <span className="quote-icon">
-          <FaQuoteRight />
-        </span>
-      </div>
-      <h4>{name}</h4>
-      <p>{job}</p>
-      <p>{description}</p>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{description}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          surprise me
+        </button>
+      </article>
     </main>
   );
 };
--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ const reviews = [
   {
     id: 2,
     name: "latoya collins",
-    job: "web designer",
+    job: "UX designer",
     image: "https://course-api.com/images/people/person-2.jpeg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -18,7 +18,7 @@ const reviews = [
   {
     id: 3,
     name: "tim hopkins",
-    job: "intern",
+    job: "product manager",
     image: "https://course-api.com/images/people/person-4.jpeg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -26,7 +26,7 @@ const reviews = [
   {
     id: 4,
     name: "brandon andrews",
-    job: "the boss",
+    job: "team lead",
     image: "https://course-api.com/images/people/person-3.jpeg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
